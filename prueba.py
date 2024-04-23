ce1d1ce9ca1066927c10f4f2ddb7ef759556f96f
Maite111 <106892417+Maite111@users.noreply.github.com>
2024-04-23 10:10:22 -0500

    prueba.py

--- prueba.py
+++ prueba.py
@@ -1,15 +1,7 @@
 # COMANDOS
 
-pip install pandas
-
 import streamlit as st
 import panda as pd
 import numpy as np
 
-# Instalar comando que permite abrir el excel
-pip install openpyxl
-import openpyxl
-
-# Abrir excel
-exceldoc = pd.read_excel("../")
-display(exceldoc())
+df = pd.read_excel('ejemplo.xlsx')
