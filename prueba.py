258e50ad1dfa8dadb41c9007079968bdd7232d67
Maite111 <106892417+Maite111@users.noreply.github.com>
2024-04-23 10:04:33 -0500

    prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,15 @@
 # COMANDOS
+
+pip install pandas
+
+import streamlit as st
+import panda as pd
+import numpy as np
+
+# Instalar comando que permite abrir el excel
+pip install openpyxl
+import openpyxl
+
+# Abrir excel
+exceldoc = pd.read_excel("../")
+display(exceldoc())
